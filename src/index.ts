cf04378b96c82cf849918882d8e5ae2aa5534f75
Joshua Ravael <jravaellnew@gmail.com>
2026-01-16 16:50:56 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Client } from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal';
+
+const client = new Client({
+    puppeteer: {
+        headless: true,
+    }
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
